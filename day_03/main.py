42c9d2f71d6a8d45ae8caed3ae6e94841f06b189
Mikael Escolin <mikaelescolin@gmail.com>
2020-10-28 11:39:32 +0000

    Update solution for part

--- day_03/main.py
+++ day_03/main.py
@@ -2,7 +2,7 @@ from types import GetSetDescriptorType
 
 
 def main():
-    with open('day_03/data_small.txt', 'r', encoding='utf-8') as file:
+    with open('day_03/data_medium_1.txt', 'r', encoding='utf-8') as file:
         wires = [line.strip().split(',') for line in file]
         wire_1, wire_2 = wires
     # END WITH
@@ -10,20 +10,21 @@ def main():
     points_1 = get_points(wire_1)
     points_2 = get_points(wire_2)
 
-    set_1 = set(points_1)
-    set_2 = set(points_2)
+    set_1 = set([point[:2] for point in points_1])
+    set_2 = set([point[:2] for point in points_2])
 
-    intersections = set_1.intersection(set_2)
-    manhattan_distances = get_manhattan_distances(intersections)
-    intersection_steps = get_steps_to_intersection(
-        wire_1, wire_2, intersections)
+    intersections = list(set_1.intersection(set_2))
 
-    print(intersection_steps[0])
+    intersections_steps = get_steps_to_intersection(
+        points_1, points_2, intersections)
+
+    print(intersections_steps[1])
 # END main()
 
 
 def get_points(wire):
-    points = [(0, 0)]
+    points = [(0, 0, 0)]
+    points_coords = [(0, 0)]
 
     last_point = points[0]
     for path in wire:
@@ -31,25 +32,33 @@ def get_points(wire):
 
         for i in range(0, steps):
             if direction == "R":
-                new_point = (last_point[0] + 1, last_point[1])
+                new_point = (last_point[0] + 1,
+                             last_point[1], last_point[2] + 1)
             elif direction == "L":
-                new_point = (last_point[0] - 1, last_point[1])
+                new_point = (last_point[0] - 1,
+                             last_point[1], last_point[2] + 1)
             elif direction == "U":
-                new_point = (last_point[0], last_point[1] + 1)
+                new_point = (
+                    last_point[0], last_point[1] + 1, last_point[2] + 1)
             elif direction == "D":
-                new_point = (last_point[0], last_point[1] - 1)
+                new_point = (
+                    last_point[0], last_point[1] - 1, last_point[2] + 1)
             else:
                 print("ERROR: invalid direction: " + direction)
                 exit()
             # END IF
 
+            if new_point[:2] in points_coords:
+                temp = list(new_point)
+                temp[2] -= 1
+                new_point = tuple(temp)
+            # END IF
+
             points.append(new_point)
+            points_coords.append(new_point[:2])
             last_point = new_point
 
-            #print(f"Point {i} added: {new_point}")
         # END FOR
-
-        # print("~~~~~~~~~~~~~~~~~~~~~~~~~~")
     # END FOR
 
     return points
@@ -68,69 +77,29 @@ def get_manhattan_distances(intersections):
 # END get_manhattan_distances()
 
 
-def get_steps_to_intersection(wire_1, wire_2, intersections):
+def get_steps_to_intersection(points_1, points_2, intersections):
     steps_1 = []
-    curr_pos = [0, 0]
-    curr_steps = 0
-    for path in wire_1:
-        direction, steps = path[:1], int(path[1:])
-
-        for i in range(0, steps):
-            curr_steps += 1
-
-            if direction == "R":
-                curr_pos[0] += 1
-            elif direction == "L":
-                curr_pos[0] -= 1
-            elif direction == "U":
-                curr_pos[1] += 1
-            elif direction == "D":
-                curr_pos[1] -= 1
-            else:
-                print("ERROR: invalid direction: " + direction)
-                exit()
-            # END IF
-
-            if tuple(curr_pos) in intersections:
-                steps_1.append(curr_steps)
-            # END IF
-        # END FOR
-    # END FOR
-
     steps_2 = []
-    curr_pos = [0, 0]
-    curr_steps = 0
-    for path in wire_2:
-        direction, steps = path[:1], int(path[1:])
-
-        for i in range(0, steps):
-            curr_steps += 1
-
-            if direction == "R":
-                curr_pos[0] += 1
-            elif direction == "L":
-                curr_pos[0] -= 1
-            elif direction == "U":
-                curr_pos[1] += 1
-            elif direction == "D":
-                curr_pos[1] -= 1
-            else:
-                print("ERROR: invalid direction: " + direction)
-                exit()
+    for intersection in intersections:
+        for point in points_1:
+            if point[:2] == intersection:
+                steps_1.append(point[2])
             # END IF
+        # END FOR
 
-            if tuple(curr_pos) in intersections:
-                steps_2.append(curr_steps)
+        for point in points_2:
+            if point[:2] == intersection:
+                steps_2.append(point[2])
             # END IF
         # END FOR
     # END FOR
 
-    intersection_steps = []
+    intersections_steps = []
     for i in range(0, len(steps_1)):
-        intersection_steps.append(steps_1[i] + steps_2[i])
+        intersections_steps.append(steps_1[i] + steps_2[i])
     # END FOR
 
-    return sorted(intersection_steps)
+    return intersections_steps
 # END get_steps_to_intersection()
 
 
